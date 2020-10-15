032734f96c735112a7b27e810d06e9aa12bf38a4
mayunlong <865147643@qq.com>
2020-10-15 18:04:59 +0800

    1014作业

--- 2020-10-14-React-3/my-app/src/stats.js
+++ 2020-10-14-React-3/my-app/src/stats.js
@@ -14,11 +14,14 @@ export default class Stats extends Component {
           />
           <span className="deleteCheck">选中全部</span>
         </label>
-        <a className="deleteCheck"
-          onClick={() => {
-            removeCheck();
-          }}
-        >删除选中项</a>
+        {
+          checkNum > 0 &&
+          <a className="deleteCheck"
+            onClick={() => {
+              removeCheck();
+            }}
+          >删除选中项</a>
+        }
         <p>{checkNum > 0 && `当前选中${checkNum}项，`} {totalNum > 0 && `共${totalNum}条留言`}</p>
       </div>
     </Fragment>
